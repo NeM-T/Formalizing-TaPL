25aa883aab2811684d898e7674bc71cba1073e89
NeM-T <persodou@keio.jp>
2020-07-10 15:24:53 +0900

    いくつかの定義の修正と証明できないなにか

--- untype_lambda/chap5.v
+++ untype_lambda/chap5.v
@@ -31,38 +31,128 @@ Compute ((fun s => fun z => z) S Z ).
 
 
 From Coq Require Import Strings.String.
-
+From Coq Require Import List.
+Import List.ListNotations.
 
 Inductive term : Type :=
   | var : string -> term
   | abs : string -> term -> term
   | app : term -> term -> term.
 
-
 Definition eqb_string (x y : string) : bool :=
   if string_dec x y then true else false.
 
+Fixpoint In (a:string) (l:list string) : bool :=
+  match l with
+    | [] => false
+    | b :: m => if eqb_string b a then true else In a m
+  end.
+
+Fixpoint FV (t : term) (l: list string) : (list string) :=
+  match t with
+  | var x => if In x l then [] else [x]
+  | abs x t1 => FV t1 (x :: l)
+  | app t1 t2 => (FV t1 l) ++ (FV t2 (l ++ (FV t1 l)))
+  end.
+
+Fixpoint size (t: term) : nat :=
+  match t with
+  | var _ => 1
+  | abs _ t1 => 1 + (size t1)
+  | app t1 t2 => (size t1) + (size t2)
+  end.
+
+Theorem eqb_string_true_iff : forall x y : string,
+    eqb_string x y = true <-> x = y.
+Proof.
+   intros x y.
+   unfold eqb_string.
+   destruct (string_dec x y) as [|Hs].
+   - subst. split. reflexivity. reflexivity.
+   - split.
+     + intros contra. discriminate contra.
+     + intros H. rewrite H in Hs. destruct Hs. reflexivity.
+Qed.
+
+Lemma le_FV : forall t l,
+    length (FV t l) <= length((FV t nil)).
+Proof.
+  induction t; intros; simpl.
+  -
+    destruct (In s l); auto.
+  -
+    admit.
+  -
+    rewrite app_length. rewrite app_length. apply PeanoNat.Nat.add_le_mono; auto.
+    clear IHt1.
+Abort.
+
+Lemma e5_3_3 : forall t,
+    le (length (FV t nil)) (size (t)).
+Proof.
+  induction t; simpl; auto.
+  -
+    apply PeanoNat.Nat.le_trans with(m:= length(FV t [])); auto.
+    admit.
+  -
+    rewrite app_length. apply PeanoNat.Nat.add_le_mono; auto.
+    apply PeanoNat.Nat.le_trans with(m:= length(FV t2 [])); auto.
+Abort.
+
+Fixpoint FV2 (t : term) (l: list string) : (list string) :=
+  match t with
+  | var s => if In s l then nil else [s]
+  | abs s t1 => FV2 t1 (s :: l)
+  | app t1 t2 => (FV2 t1 l) ++ (FV2 t2 l)
+  end.
+
+
+Lemma e5_3_3 : forall t,
+    le (length (FV2 t nil)) (size (t)).
+Proof.
+  induction t; simpl; auto.
+  -
+    apply PeanoNat.Nat.le_trans with(m:= length(FV2 t [])); auto.
+    clear.
+    induction t; simpl; auto.
+    +
+      destruct (eqb_string s s0); auto.
+    +
+      admit.
+    +
+      rewrite app_length. rewrite app_length.
+      apply PeanoNat.Nat.add_le_mono; auto.
+  -
+    rewrite app_length. apply PeanoNat.Nat.add_le_mono; auto.
+Abort.
+
 Reserved Notation "'[' x ':=' s ']' t" (at level 20).
 
-(*
-       [x:=s]x = s
-       [x:=s]y = y if x <> y
-       [x:=s](\x:T11. t12) = \x:T11. t12
-       [x:=s](\y:T11. t12) = \y:T11. [x:=s]t12 if x <> y
-       [x:=s](t1 t2) = ([x:=s]t1) ([x:=s]t2)
-*)
-
-Fixpoint subst (x : string) (s : term) (t : term) : term :=
+Fixpoint subst (x : string) (s : term) (t : term) : option term :=
   match t with
   | var x' =>
-      if eqb_string x x' then s else t
-  | abs x' t1 =>
-      abs x'  (if eqb_string x x' then t1 else ([x:=s] t1))
+      if eqb_string x x' then Some s else Some t
+  | abs y t1 =>
+    if eqb_string x y then Some (abs y t1) else
+      if In y (FV s []) then None else
+        match [x:= s] t1 with
+        |Some t' => Some (abs y t')
+        | None => None
+        end
   | app t1 t2 =>
-      app ([x:=s] t1) ([x:=s] t2)
+    match [x:= s] t1 with
+    | Some t1' =>
+      match ([x:=s] t2) with
+      | Some t2' => Some (app t1' t2')
+      | None => None
+      end
+    | None => None
+    end
   end
 
 where "'[' x ':=' s ']' t" := (subst x s t).
+
+Compute (["x":= (app (var "y")(var "z"))] (abs "w" (app (var "x") (var "w")))).
 
 Inductive value : term -> Prop :=
   | v_abs : forall x t,
@@ -71,9 +161,10 @@ Inductive value : term -> Prop :=
 Reserved Notation "t1 '-->' t2" (at level 40).
 
 Inductive eval : term -> term -> Prop :=
-  | E_AppAbs : forall x t12 v2,
+  | E_AppAbs : forall x t12 v2 t',
          value v2 ->
-         (app (abs x t12) v2) --> [x:=v2]t12
+         [x:= v2]t12 = Some t' ->
+         (app (abs x t12) v2) --> t'
   | E_App1 : forall t1 t1' t2,
          t1 --> t1' ->
          app t1 t2 --> app t1' t2
@@ -104,3 +195,22 @@ Definition z := "z".
 Hint Unfold x.
 Hint Unfold y.
 Hint Unfold z.
+
+Lemma determin  : forall t t' t'',
+    t --> t' -> t --> t'' -> t' = t''.
+Proof.
+  intros. generalize dependent t''. induction H; intros.
+  -
+    inversion H1; subst .
+    rewrite H0 in H7. inversion H7; auto.
+    inversion H5. inversion H; subst. inversion H6.
+  -
+    inversion H0; subst. inversion H.
+    apply IHeval in H4. rewrite H4; auto. inversion H3; subst. inversion H.
+  -
+    inversion H1; subst. inversion H4; subst. inversion H0.
+    inversion H; subst. inversion H5.
+    apply IHeval in H6. rewrite H6; auto.
+Qed.
+
+Close Scope string_scope.
--- untype_lambda/chap6.v
+++ untype_lambda/chap6.v
@@ -3,27 +3,58 @@ Inductive term : Type :=
 | Abs : term -> term
 | App : term -> term -> term.
 
+Notation beta_ShiftNum := 0.
+
 Fixpoint shift (d c: nat) (t: term) : term :=
   match t with
   | Var k =>
-    if Nat.leb c k then Var (k + d) else Var k
+    if Nat.leb c k then
+      match d with
+      | beta_ShiftNum => Var (k - 1)
+      | _ => Var (k + d)
+      end
+    else Var k
   | Abs t1 => Abs (shift d (c + 1) t1)
   | App t1 t2 => App (shift d c t1) (shift d c t2)
   end.
 
+Definition up (n: nat) (t: term) : term := shift n 0 t.
 
-Compute ( shift 2 0 (Abs (Abs (App (Var 1) (App (Var 0) (Var 2) ))))).
-Compute ( shift 2 0 (Abs (App (Var 0) (App (Var 1)(Abs (App (Var 0)(App (Var 1)(Var 2)))))))).
+Compute ( up 2 (Abs (Abs (App (Var 1) (App (Var 0) (Var 2) ))))). (* = Abs (Abs (App (Var 1) (App (Var 0) (Var 4)))) *)
+Compute ( up 2 (Abs (App (Var 0) (App (Var 1)(Abs (App (Var 0)(App (Var 1)(Var 2)))))))). (* = Abs (App (Var 0) (App (Var 3) (Abs (App (Var 0) (App (Var 1) (Var 4)))))) *)
 
 Fixpoint subst (j: nat) (s t: term) : term :=
   match t with
   | Var k =>
     if Nat.eqb j k then s else Var k
   | Abs t1 =>
-    Abs (subst (j + 1) (shift 1 0 s) t1)
+    Abs (subst (j + 1) (up 1 s) t1)
   | App t1 t2 =>
     App (subst j s t1) (subst j s t2)
   end.
 
-Compute (subst 0 (Var 1) (App (Var 0) (Abs(Abs (Var 2))))).
-Compute (subst 0 (App (Var 1) (Abs (Var 2))) (App (Var 0) (Abs (Var 1)))).
+Compute (subst 0 (Var 1) (App (Var 0) (Abs(Abs (Var 2))))).  (* = App (Var 1) (Abs (Abs (Var 3))) *)
+Compute (subst 0 (App (Var 1) (Abs (Var 2))) (App (Var 0) (Abs (Var 1)))). (* = App (App (Var 1) (Abs (Var 2))) (Abs (App (Var 2) (Abs (Var 3)))) *)
+
+Reserved Notation " t '-->' t' " (at level 40).
+
+Inductive value : term -> Prop :=
+  | v_abs : forall n,
+      value (Abs n).
+
+Notation "'[' x ':=' s ']' t" := (subst x s t) (at level 20).
+
+Inductive eval : term -> term -> Prop :=
+  | E_App1 : forall t1 t1' t2,
+         t1 --> t1' ->
+         App t1 t2 --> App t1' t2
+  | E_App2 : forall v1 t2 t2',
+         value v1 ->
+         t2 --> t2' ->
+         App v1 t2 --> App v1 t2'
+  | E_AppAbs : forall t1 v2,
+         value v2 ->
+         App (Abs t1) v2 -->
+             up beta_ShiftNum ([0 := up 1 (v2)] t1)
+
+  where " t '-->' t' " := (eval t t').
